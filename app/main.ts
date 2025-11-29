e2cbc2aaf63d91244d6bbdef5b67cef16b301d9b
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2025-11-29 12:23:57 +0530

    Implement echo

--- app/main.ts
+++ app/main.ts
@@ -5,15 +5,27 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-const prompt = () => {
-  rl.question("$ ", (command) => {
-    if (command === "exit") {
-      rl.close();
-      return;
-    }
-    console.log(`${command}: command not found`);
-    prompt();
-  });
-};
+rl.setPrompt("$ ");
+rl.prompt();
 
-prompt();
+rl.on("line", (line) => {
+  const [command, ...args] = line.trim().split(/\s+/);
+
+  switch (command) {
+    case "exit":
+      rl.close();
+      break;
+    case "echo":
+      console.log(args.join(" "));
+      break;
+    default:
+      console.log(`${command}: command not found`);
+      break;
+  }
+
+  rl.prompt();
+});
+
+rl.on("close", () => {
+  process.exit(0);
+});
